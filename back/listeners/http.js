4b3458850768d93a49a56e274f853fd4269a8b13
Dejan Tomic <hi@iamdejan.com>
2026-07-11 16:09:37 +0200

    listeners: http session state

---
+++ back/listeners/http.js
@@ -0,0 +1,29 @@
+import onetype from '@onetype/framework';
+import auth from '../addon.js';
+
+onetype.MiddlewareIntercept('servers.http.request', async (middleware) =>
+{
+	const http = middleware.value;
+	const path = http.url.pathname;
+
+	if(path.endsWith('.js') || path.endsWith('.css'))
+	{
+		return await middleware.next();
+	}
+
+	const value = onetype.CookieGet('ot_session', http.request) || http.request.headers.authorization;
+
+	if(!value)
+	{
+		return await middleware.next();
+	}
+
+	const session = await auth.Fn('session', value);
+
+	if(session)
+	{
+		http.state.user = { ...session.user, team: session.team };
+	}
+
+	await middleware.next();
+});
